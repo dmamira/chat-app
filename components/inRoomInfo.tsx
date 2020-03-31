5dbb5de15e1dbbbedc1db904b00629bfd1968fbf
dmamira <49590399+dmamira@users.noreply.github.com>
2020-03-31 18:19:27 +0900

    next coreをアップグレード

---
+++ components/inRoomInfo.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+interface Props {
+    name:string,
+    description:string
+}
+class InRoomInfo extends React.Component<Props, any> {
+    render() {
+        return (
+            <div>
+                <p className="roomName">{this.props.name}</p>
+                <p className="description">{this.props.description}</p>
+                {/*language=CSS*/}
+                <style jsx>{`
+                    .roomName {
+                        font-weight: bold;
+                        font-size: 20px;
+                        margin-top:10px
+                    }
+                    .description{
+                    margin-top:10px
+                    }
+
+                    div {
+                        max-width: 400px;
+                        box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 5px;
+                        border-radius: 10px;
+                    }
+                    p{
+                    margin:0px
+                    }
+                `}</style>
+            </div>
+        )
+    }
+}
+
+export default InRoomInfo;
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,32 +1,12 @@
 import * as React from 'react';
+import InRoomInfo from "../components/inRoomInfo";
 
 class Test extends React.Component<any, any> {
     render() {
         return (
-            <div>
-                <p className="roomName">text goes here</p>
-                <p className="description">ここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入ります</p>
-                {/*language=CSS*/}
-                <style jsx>{`
-                    .roomName {
-                        font-weight: bold;
-                        font-size: 20px;
-                        margin-top:10px
-                    }
-                    .description{
-                    margin-top:10px
-                    }
-
-                    div {
-                        max-width: 400px;
-                        box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 5px;
-                        border-radius: 10px;
-                    }
-                    p{
-                    margin:0px
-                    }
-                `}</style>
-            </div>
+          <div>
+              <InRoomInfo name="荒野の部屋" description="荒野行動について語り合う部屋です。荒野行動がお好きな方はお越しください。"/>
+          </div>
         )
     }
 }
